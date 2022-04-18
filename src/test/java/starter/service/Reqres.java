a85862c71f97a20bdfd27ed7457ec2dae411ac25
luhooor <ylk.osd@gmail.com>
2022-04-19 01:18:25 +0700

    added methods for getting single user data

--- src/test/java/starter/service/Reqres.java
+++ src/test/java/starter/service/Reqres.java
@@ -1,8 +1,15 @@
 package starter.service;
 
+import net.serenitybdd.rest.SerenityRest;
+
 public class Reqres {
     private static final String BASE_URL = "https://reqres.in/api";
 
+    public void getSingleUser(int id){
+        SerenityRest.get(BASE_URL+"/users/"+id);
+    }
 
-
+    public void getSingleUser(String value){
+        SerenityRest.get(BASE_URL+"/users/"+value);
+    }
 }
