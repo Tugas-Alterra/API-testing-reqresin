02b0fc32550c726e2a4af302bc0219e7c321c622
Meilisa Putri <meilisaputridwicahya@gmail.com>
2022-04-19 20:35:03 +0700

    added login feature

--- src/test/java/starter/stepdefinitions/LoginStepDefinitions.java
+++ src/test/java/starter/stepdefinitions/LoginStepDefinitions.java
@@ -1,2 +1,13 @@
-package starter.stepdefinitions;public class LoginStepDefinitions {
+package starter.stepdefinitions;
+
+import io.cucumber.java.en.And;
+
+public class LoginStepDefinitions {
+    @And("user send POST success login request to reqresin")
+    public void userSendPOSTSuccessLoginRequestToReqresin() {
+    }
+
+    @And("user send POST unsuccess login request to reqresin")
+    public void userSendPOSTUnsuccessLoginRequestToReqresin() {
+    }
 }
